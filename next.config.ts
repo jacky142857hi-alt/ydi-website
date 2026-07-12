25d03dbcaf396baf4867416cd46566d8975b93c7
jacky142857hi-alt <jacky142857hi@gmail.com>
2026-07-13 01:45:09 +0800

    Add basePath for Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/ydi-website" : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
